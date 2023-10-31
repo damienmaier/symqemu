11a8d81ce703ca1c1d5ac9c6eb2f9c9f908d793a
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-10-31 10:38:19 +0000

    improve test script

--- tests/symqemu/util.py
+++ tests/symqemu/util.py
@@ -8,15 +8,21 @@ BINARIES_DIR = pathlib.Path(__file__).parent / "binaries"
 def run_symqemu_on_test_binary(binary_name: str, output_dir: pathlib.Path) -> None:
     binary_dir = BINARIES_DIR / binary_name
 
-    subprocess.run(
-        [
-            str(SYMQEMU_EXECUTABLE),
-            str(binary_dir / 'binary'),
-            str(binary_dir / 'input')
-        ],
-        env={
-            'SYMCC_OUTPUT_DIR': str(output_dir),
-            'SYMCC_INPUT_FILE': str(binary_dir / 'input')
-        }
-    )
+    command = [
+        str(SYMQEMU_EXECUTABLE),
+        str(binary_dir / 'binary'),
+        str(binary_dir / 'input')
+    ]
 
+    environment_variables = {
+        'SYMCC_OUTPUT_DIR': str(output_dir),
+        'SYMCC_INPUT_FILE': str(binary_dir / 'input')
+    }
+
+    print(f'about to run command: {" ".join(command)}')
+    print(f'with environment variables: {environment_variables}')
+
+    subprocess.run(
+        command,
+        env=environment_variables
+    )
