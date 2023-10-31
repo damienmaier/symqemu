31084b08bb72283a7bc770f1a8f890330e04ddb4
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-10-31 15:08:54 +0000

    allow test binaries to take args

--- tests/symqemu/util.py
+++ tests/symqemu/util.py
@@ -8,11 +8,18 @@ BINARIES_DIR = pathlib.Path(__file__).parent / "binaries"
 def run_symqemu_on_test_binary(binary_name: str, output_dir: pathlib.Path) -> None:
     binary_dir = BINARIES_DIR / binary_name
 
-    command = [
-        str(SYMQEMU_EXECUTABLE),
-        str(binary_dir / 'binary'),
-        str(binary_dir / 'input')
-    ]
+    with open(binary_dir / 'args', 'r') as f:
+        binary_args = f.read().strip().split(' ')
+
+    def replace_placeholder_with_input(arg: str):
+        return str(binary_dir / 'input') if arg == '@@' else arg
+
+    binary_args = *map(replace_placeholder_with_input, binary_args),
+
+    command = (
+                  str(SYMQEMU_EXECUTABLE),
+                  str(binary_dir / 'binary'),
+              ) + binary_args
 
     environment_variables = {
         'SYMCC_OUTPUT_DIR': str(output_dir),
