761825cfc865362f8bd64e1c45ab450fd7df0959
Ubuntu <91182145+damienmaier@users.noreply.github.com>
2023-10-31 10:38:19 +0000

    improve test script

--- tests/symqemu/test.py
+++ tests/symqemu/test.py
@@ -1,8 +1,8 @@
 import filecmp
 import pathlib
 import shutil
-import subprocess
 import unittest
+
 import util
 
 
@@ -15,12 +15,21 @@ class SymQemuTests(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.SYMQEMU_OUTPUT_DIR)
 
-    def test_simple(self):
+    def run_symqemu_and_assert_correct_result(self, binary_name):
 
-        util.run_symqemu_on_test_binary(binary_name='simple', output_dir=self.SYMQEMU_OUTPUT_DIR)
+        util.run_symqemu_on_test_binary(binary_name=binary_name, output_dir=self.SYMQEMU_OUTPUT_DIR)
 
-        expected_vs_actual_output_comparison = filecmp.dircmp(self.SYMQEMU_OUTPUT_DIR, util.BINARIES_DIR / 'simple' / 'expected_outputs')
+        # `filecmp.dircmp` does a "shallow" comparison, but this is not a problem here because
+        # the timestamps should always be different, so the actual content of the files will be compared.
+        # See https://docs.python.org/3/library/filecmp.html#filecmp.dircmp
+        expected_vs_actual_output_comparison = filecmp.dircmp(self.SYMQEMU_OUTPUT_DIR, util.BINARIES_DIR / binary_name / 'expected_outputs')
         self.assertEqual(expected_vs_actual_output_comparison.diff_files, [])
         self.assertEqual(expected_vs_actual_output_comparison.left_only, [])
         self.assertEqual(expected_vs_actual_output_comparison.right_only, [])
         self.assertEqual(expected_vs_actual_output_comparison.funny_files, [])
+
+    def test_simple(self):
+        self.run_symqemu_and_assert_correct_result('simple')
+
+    def test_printf(self):
+        self.run_symqemu_and_assert_correct_result('printf')
