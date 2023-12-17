f42df672525fc3c8f85c8657ec7c932c5d49358c
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2023-12-17 22:00:49 +0000

    refactoring

--- tests/symqemu/util.py
+++ tests/symqemu/util.py
@@ -1,9 +1,40 @@
 import pathlib
 import subprocess
+import symcctrace
+import pickle
 
 SYMQEMU_RUN_STDOUT_STDERR = pathlib.Path('/tmp/symqemu_stdout_stderr')
 SYMQEMU_EXECUTABLE = pathlib.Path(__file__).parent.parent.parent / "build" / "x86_64-linux-user" / "qemu-x86_64"
 BINARIES_DIR = pathlib.Path(__file__).parent / "binaries"
+
+
+def run_symqemu(
+        qemu_executable: pathlib.Path,
+        qemu_additional_args: list[str],
+        binary: pathlib.Path,
+        binary_arguments: list[str],
+        generated_test_cases_output_dir: pathlib.Path,
+        symbolized_input_file: pathlib.Path = None,
+        binary_stdin=None,
+):
+    command = str(qemu_executable), *qemu_additional_args, str(binary), *binary_arguments
+
+    environment_variables = {'SYMCC_OUTPUT_DIR': str(generated_test_cases_output_dir)}
+    if symbolized_input_file is not None:
+        environment_variables['SYMCC_INPUT_FILE'] = str(symbolized_input_file)
+
+    print(f'about to run command: {" ".join(command)}')
+    print(f'with environment variables: {environment_variables}')
+
+    with open(SYMQEMU_RUN_STDOUT_STDERR, 'w') as f:
+        subprocess.run(
+            command,
+            env=environment_variables,
+            stdout=f,
+            stderr=subprocess.STDOUT,
+            input=binary_stdin
+        )
+
 
 
 def run_symqemu_on_test_binary(
@@ -24,25 +55,46 @@ def run_symqemu_on_test_binary(
     with open(binary_dir / 'args', 'r') as f:
         binary_args = f.read().strip().split(' ')
 
-    def replace_placeholder_with_input(arg: str):
+    def replace_placeholder_with_input(arg: str) -> str:
         return str(binary_dir / 'input') if arg == '@@' else arg
 
-    binary_args = *map(replace_placeholder_with_input, binary_args),
+    binary_args = list(map(replace_placeholder_with_input, binary_args))
 
-    command = str(qemu_executable), *additional_args.split(), str(binary_dir / 'binary'), *binary_args
+    run_symqemu(
+        qemu_executable=qemu_executable,
+        qemu_additional_args=additional_args.split(' '),
+        binary=binary_dir / 'binary',
+        binary_arguments=binary_args,
+        generated_test_cases_output_dir=generated_test_cases_output_dir,
+        symbolized_input_file=binary_dir / 'input',
+    )
 
-    environment_variables = {
-        'SYMCC_OUTPUT_DIR': str(generated_test_cases_output_dir),
-        'SYMCC_INPUT_FILE': str(binary_dir / 'input')
-    }
 
-    print(f'about to run command: {" ".join(command)}')
-    print(f'with environment variables: {environment_variables}')
+BACKEND_TRACE_FILE = pathlib.Path('/tmp/backend_trace.json')
+SYMQEMU_TRACE_ADDRESSES_FILE = pathlib.Path('/tmp/symqemu_addresses.json')
 
-    with open(SYMQEMU_RUN_STDOUT_STDERR, 'w') as f:
-        subprocess.run(
-            command,
-            env=environment_variables,
-            stdout=f,
-            stderr=subprocess.STDOUT,
-        )
+
+def run_symqemu_and_save_trace_data(
+        binary_name: str,
+        qemu_executable: pathlib.Path,
+        destination_directory: pathlib.Path,
+        qemu_additional_args: str = '',
+) -> None:
+    # Make sure that we will get an error if the files are not created
+    BACKEND_TRACE_FILE.unlink(missing_ok=True)
+    SYMQEMU_TRACE_ADDRESSES_FILE.unlink(missing_ok=True)
+    SYMQEMU_RUN_STDOUT_STDERR.unlink(missing_ok=True)
+
+    run_symqemu_on_test_binary(
+        binary_name=binary_name,
+        qemu_executable=qemu_executable,
+        additional_args=qemu_additional_args,
+
+    )
+    trace_data = symcctrace.parse_trace(trace_file=BACKEND_TRACE_FILE,
+                                        memory_region_names_file=SYMQEMU_TRACE_ADDRESSES_FILE)
+
+    with open(destination_directory / 'trace.pickle', 'wb') as f:
+        pickle.dump(trace_data, f)
+
+    SYMQEMU_RUN_STDOUT_STDERR.rename(destination_directory / 'stdout_stderr.txt')
